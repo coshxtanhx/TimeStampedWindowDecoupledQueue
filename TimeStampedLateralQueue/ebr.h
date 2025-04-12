63fe6bc8611887f5fa26366e7a48282a936da94c
Cho Yong Joon <yzz1108j@gmail.com>
2025-04-13 03:21:11 +0900

    refactor: EBR - false sharing prevention

--- TimeStampedLateralQueue/ebr.h
+++ TimeStampedLateralQueue/ebr.h
@@ -9,16 +9,26 @@
 #include "my_thread.h"
 
 namespace lf {
+	struct alignas(std::hardware_destructive_interference_size) Reservation {
+		Reservation() = default;
+
+		void StartOP(std::atomic<uint64_t>& base_epoch) {
+			epoch = base_epoch.fetch_add(1, std::memory_order_relaxed);
+		}
+
+		void EndOp() {
+			epoch = std::numeric_limits<uint64_t>::max();
+		}
+
+		volatile uint64_t epoch{ std::numeric_limits<uint64_t>::max() };
+	};
+
 	template<class T>
 	class EBR {
 	public:
 		EBR() = delete;
 		EBR(int num_thread) noexcept
-			: num_thread_{ num_thread }, reservations_{ new RetiredEpoch[num_thread] }
-			, retired_(num_thread) {
-			for (int i = 0; i < num_thread; ++i) {
-				reservations_[i] = std::numeric_limits<RetiredEpoch>::max();
-			}
+			: num_thread_{ num_thread }, reservations_(num_thread), retired_(num_thread) {
 		}
 		~EBR() noexcept {
 			for (auto& q : retired_) {
@@ -28,59 +38,60 @@ namespace lf {
 					delete f;
 				}
 			}
-			delete[] reservations_;
 		}
 		EBR(const EBR&) = delete;
 		EBR(EBR&&) = delete;
 		EBR& operator=(const EBR&) = delete;
 		EBR& operator=(EBR&&) = delete;
+
 		void Retire(T* ptr) noexcept {
 			ptr->retire_epoch = epoch_.load(std::memory_order_relaxed);
-			retired_[MyThread::GetID()].push(ptr);
-			if (retired_[MyThread::GetID()].size() >= GetCapacity()) {
+			retired_[thread::ID()].push(ptr);
+			if (retired_[thread::ID()].size() >= GetCapacity()) {
 				Clear();
 			}
 		}
+
 		void StartOp() noexcept {
-			reservations_[MyThread::GetID()] = epoch_.fetch_add(1, std::memory_order_relaxed);
+			reservations_[thread::ID()].StartOP(epoch_);
 		}
+
 		void EndOp() noexcept {
-			reservations_[MyThread::GetID()] = std::numeric_limits<RetiredEpoch>::max();
+			reservations_[thread::ID()].EndOp();
 		}
 
 	private:
-		using RetiredEpoch = uint64_t;
-		using RetiredNodeQueue = std::queue<T*>;
+		struct alignas(std::hardware_destructive_interference_size) RetiredPtrQueue : std::queue<T*> {};
 
-		constexpr RetiredEpoch GetCapacity() const noexcept {
-			return static_cast<RetiredEpoch>(num_thread_ * 60);
+		uint64_t GetCapacity() const noexcept {
+			return static_cast<uint64_t>(num_thread_ * 60);
 		}
 
-		RetiredEpoch GetMinReservation() const noexcept {
-			auto min_re = std::numeric_limits<RetiredEpoch>::max();
-			for (int i = 0; i < num_thread_; ++i) {
-				min_re = std::min(min_re, reservations_[i]);
-			}
-			return min_re;
+		uint64_t GetMinReservation() const noexcept {
+			return std::min_element(reservations_.begin(), reservations_.end(),
+				[](const Reservation& a, const Reservation& b) {
+					return a.epoch < b.epoch;
+				})->epoch;
 		}
 
 		void Clear() noexcept {
-			RetiredEpoch max_safe_epoch = GetMinReservation();
+			auto max_safe_epoch = GetMinReservation();
 
-			while (false == retired_[MyThread::GetID()].empty()) {
-				auto f = retired_[MyThread::GetID()].front();
-				if (f->retire_epoch >= max_safe_epoch)
+			while (false == retired_[thread::ID()].empty()) {
+				auto f = retired_[thread::ID()].front();
+				if (f->retire_epoch >= max_safe_epoch) {
 					break;
-				retired_[MyThread::GetID()].pop();
+				}
+				retired_[thread::ID()].pop();
 
 				delete f;
 			}
 		}
 
 		int num_thread_;
-		RetiredEpoch* volatile reservations_;
-		std::vector<RetiredNodeQueue> retired_;
-		std::atomic<RetiredEpoch> epoch_;
+		std::vector<Reservation> reservations_;
+		std::vector<RetiredPtrQueue> retired_;
+		std::atomic<uint64_t> epoch_;
 	};
 }
 

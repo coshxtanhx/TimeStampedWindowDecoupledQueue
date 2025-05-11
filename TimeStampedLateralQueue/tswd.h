ccdae57a41742ba71c584e3407894590b61cc71e
Cho Yong Joon <yzz1108j@gmail.com>
2025-05-12 04:02:55 +0900

    refactor: TSWD - remove dir

--- TimeStampedLateralQueue/tswd.h
+++ TimeStampedLateralQueue/tswd.h
@@ -124,8 +124,6 @@ namespace lf::tswd {
 		std::optional<int> Deq() {
 			std::vector<Node*> old_heads(queues_.size());
 			size_t id = MyThread::GetID();
-			constexpr std::array<int, 16> coprimes{ 1, 5, 7, 11, 13, 17, 19, 23, 25, 29, 31, 35, 37, 41, 43, 47 };
-			auto dir = coprimes[Random::Get(0, coprimes.size() - 1)];
 
 			ebr_.StartOp();
 			while (true) {
@@ -142,13 +140,13 @@ namespace lf::tswd {
 						ebr_.EndOp();
 						return value;
 					}
-					id = (id + dir) % queues_.size();
+					id = (id + 1) % queues_.size();
 				}
 
 				if (queues_.size() == cnt_empty) {
 					bool is_empty{ true };
 					for (size_t i = 1; i < queues_.size(); ++i) {
-						id = (i * dir + MyThread::GetID()) % queues_.size();
+						id = (i + MyThread::GetID()) % queues_.size();
 						auto next = old_heads[id]->next;
 						if (nullptr != next) {
 							is_empty = false;
